3cbbb77bbaa283582dc02c679d243c017669d81c
Andriy Hladkevych <andriyglad@gmail.com>
2022-11-03 11:20:05 +0200

    Implemented products list with categories

---
+++ src/components/CategoryFilter.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import categoriesFromServer from '../api/categories';
+
+type Props = {
+  filtersByCategory: number[],
+  setFiltersByCategory: React.Dispatch<React.SetStateAction<number[]>>,
+};
+
+export const CategoryFilter: React.FC<Props> = ({
+  filtersByCategory,
+  setFiltersByCategory,
+}) => {
+  return (
+    <div className="panel-block is-flex-wrap-wrap">
+      <a
+        href="#/"
+        data-cy="AllCategories"
+        className="button is-success mr-6 is-outlined"
+        onClick={() => {
+          setFiltersByCategory([]);
+        }}
+      >
+        All
+      </a>
+
+      {categoriesFromServer.map(category => {
+        const { id, title } = category;
+
+        return (
+          <a
+            data-cy="Category"
+            className={classNames(
+              'button',
+              'mr-1',
+              'my-1',
+              {
+                'is-info': filtersByCategory.includes(id),
+              },
+            )}
+            href="#/"
+            key={id}
+            onClick={() => {
+              setFiltersByCategory(currentFilters => {
+                if (!filtersByCategory.includes(id)) {
+                  return ([
+                    ...currentFilters,
+                    id,
+                  ]);
+                }
+
+                return currentFilters
+                  .filter(categoryId => categoryId !== id);
+              });
+            }}
+          >
+            {title}
+          </a>
+        );
+      })}
+    </div>
+  );
+};
